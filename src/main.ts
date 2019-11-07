301767c8617daaccc3eb9f6203e865faf78ab211
Petr Pliska <petr.pliska@post.cz>
2019-11-07 13:37:41 +0100

    Add graceful shutdown

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+const { createTerminus } = require('@godaddy/terminus');
 import {Response, Request} from "express"
 import {useExpressServer} from "routing-controllers"
 import express = require('express')
 const app: express.Application = express()
 const port = 8080
+import * as http from 'http'
 
 useExpressServer(app, {
     defaults: {
@@ -16,8 +18,19 @@ useExpressServer(app, {
     controllers: [__dirname + "/interfaces/rest/controllers/**/*.+(js|ts)"]
 })
 
+const server = http.createServer(app);
+
+createTerminus(server, {
+    onShutdown: () => {
+        console.log('server is starting cleanup');
+        return Promise.all([
+            // your clean logic, like closing database connections
+        ])
+    }
+})
+
 // start the express server
-app.listen( port, () => {
+server.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` )
 } )
